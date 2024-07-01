7938ac75018bf782d93762a08be1e9ccd69d87fe
Manu <mandrescabral@gmail.com>
2024-07-01 02:24:43 -0300

    refactor: remove unused import in clips.module.ts

--- src/clips/clips.module.ts
+++ src/clips/clips.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ClipsService } from './clips.service';
 import { ClipsController } from './clips.controller';
-import { Clip, ClipSchema } from 'src/schemas/clip.schema';
+import { ClipSchema } from 'src/schemas/clip.schema';
 
 @Module({
   imports: [
