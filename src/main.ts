734ebcda9367d133faf80807fc973ed5816f0ff1
Manu <mandrescabral@gmail.com>
2024-07-02 13:05:08 -0300

    feat: add morgan package

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import { configureGlobalPipes } from './common/config/global-pipes.config';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: console,
   });
+  const configService = app.get(ConfigService);
   configureGlobalPipes(app);
   app.setGlobalPrefix('api/v1');
-  await app.listen(3000);
+  app.use(morgan(configService.get<string>('MORGAN_CONFIG', 'dev')));
+  await app.listen(configService.get<number>('PORT', 3000));
 }
 bootstrap();
