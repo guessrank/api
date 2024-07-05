7739884bb13b59a0b35e8d85369c1efadb31e1f8
Manu <mandrescabral@gmail.com>
2024-07-05 11:27:34 -0300

    feat: update main.ts to enable CORS with specific origin and credentials

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,10 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   configureGlobalPipes(app);
   app.setGlobalPrefix('api/v1');
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000', // dev
+    credentials: true,
+  });
   app.use(morgan(configService.get<string>('MORGAN_CONFIG', 'dev')));
   await app.listen(configService.get<number>('PORT', 3000));
 }
