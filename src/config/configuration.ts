389e94e99dd1a20c8d5d4e87f5d09dddf7cdc844
Manu <mandrescabral@gmail.com>
2024-07-06 14:37:33 -0300

    refactor: update database_uri in configuration.ts to use MONGODB_URI environment variable

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,8 +1,7 @@
 export default () => ({
   mode: process.env.NODE_ENV || 'development',
   port: parseInt(process.env.PORT, 10) || 3000,
-  database_uri: process.env.DATABASE_URI,
-  database_name: process.env.DATABASE_NAME,
+  database_uri: process.env.MONGODB_URI,
   cors: {
     origin: 'http://localhost:3000', // dev
     credentials: true,
