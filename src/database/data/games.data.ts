acaa8e253c0ac722e0f7d1dcb85f8d4767aeada2
Manu <mandrescabral@gmail.com>
2024-07-19 11:15:18 -0300

    refactor: update gamesData to use Game type

--- src/database/data/games.data.ts
+++ src/database/data/games.data.ts
@@ -1,4 +1,5 @@
+import { Game } from 'src/schemas/game.schema';
 import { rocketLeagueData } from './rocketleague.data';
 import { valorantData } from './valorant.data';
 
-export const gamesData = [rocketLeagueData, valorantData];
+export const gamesData: Game[] = [rocketLeagueData, valorantData];
